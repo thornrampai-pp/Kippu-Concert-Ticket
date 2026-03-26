961dd5c9577ad987ac51b6987d197b793dd20dc4
thornrampai <thornrampai.t@gmail.com>
2026-03-27 02:43:25 +0700

    [Fix] deploy

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import { ENV } from '../config/env';
 
 const axiosInstance = axios.create({
   // ดึง URL จาก .env หรือใช้ค่าเริ่มต้น
-  baseURL: ENV.API_URL,
+  baseURL: '/api/v1',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
